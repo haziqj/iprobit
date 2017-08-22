9955dfbb4d16c71cbee21e014a0f72cee2f6944f
Haziq Jamil <h.jamil@lse.ac.uk>
2017-08-22 19:45:57 +0100

    Commented the wrong file

--- tests/testthat/test-7methods.R
+++ tests/testthat/test-7methods.R
@@ -1,62 +1,62 @@
-# context("Methods for iprobitMod objects")
-#
-# # Update/refit -----------------------------------------------------------------
-#
-# test_that("Update from formula", {
-#
-#   dat <- gen_mixture(10)
-#   mod.original <- mod <- iprobit(y ~ ., dat, control = list(maxit = 1),
-#                                  silent = TRUE)
-#   mod <- iprobit(mod, maxit = 5)
-#
-#   expect_s3_class(mod, "iprobitMod")
-#   expect_equal(mod$niter, 6)
-#   expect_equal(length(mod$lower.bound), 6)
-#   expect_equal(length(mod$error), 6)
-#   expect_equal(length(mod$brier), 6)
-#   expect_equal(mod$formula, mod.original$formula)
-#
-# })
-#
-# test_that("Update from non-formula", {
-#
-#   dat <- gen_mixture(10)
-#   mod.original <- mod <- iprobit(dat$y, dat$X, control = list(maxit = 1),
-#                                  silent = TRUE)
-#   mod <- iprobit(mod, maxit = 5)
-#
-#   expect_s3_class(mod, "iprobitMod")
-#   expect_equal(mod$niter, 6)
-#   expect_equal(length(mod$lower.bound), 6)
-#   expect_equal(length(mod$error), 6)
-#   expect_equal(length(mod$brier), 6)
-#   expect_equal(mod$formula, mod.original$formula)
-#
-# })
-#
-# test_that("Update options", {
-#
-#   dat <- gen_mixture(10)
-#   mod.original <- mod <- iprobit(dat$y, dat$X, control = list(maxit = 1),
-#                                  silent = TRUE)
-#
-#   expect_message(mod <- iprobit(mod))
-#   expect_output(mod <- iprobit(mod, stop.crit = 1e-1, maxit = 5, silent = FALSE))
-#
-# })
-#
-# test_that("The function update()", {
-#
-#   dat <- gen_mixture(10)
-#   mod.original <- mod <- iprobit(dat$y, dat$X, control = list(maxit = 1),
-#                                  silent = TRUE)
-#   update(mod, maxit = 5)
-#
-#   expect_s3_class(mod, "iprobitMod")
-#   expect_equal(mod$niter, 6)
-#   expect_equal(length(mod$lower.bound), 6)
-#   expect_equal(length(mod$error), 6)
-#   expect_equal(length(mod$brier), 6)
-#   expect_equal(mod$formula, mod.original$formula)
-#
-# })
+context("Methods for iprobitMod objects")
+
+# Update/refit -----------------------------------------------------------------
+
+test_that("Update from formula", {
+
+  dat <- gen_mixture(10)
+  mod.original <- mod <- iprobit(y ~ ., dat, control = list(maxit = 1),
+                                 silent = TRUE)
+  mod <- iprobit(mod, maxit = 5)
+
+  expect_s3_class(mod, "iprobitMod")
+  expect_equal(mod$niter, 6)
+  expect_equal(length(mod$lower.bound), 6)
+  expect_equal(length(mod$error), 6)
+  expect_equal(length(mod$brier), 6)
+  expect_equal(mod$formula, mod.original$formula)
+
+})
+
+test_that("Update from non-formula", {
+
+  dat <- gen_mixture(10)
+  mod.original <- mod <- iprobit(dat$y, dat$X, control = list(maxit = 1),
+                                 silent = TRUE)
+  mod <- iprobit(mod, maxit = 5)
+
+  expect_s3_class(mod, "iprobitMod")
+  expect_equal(mod$niter, 6)
+  expect_equal(length(mod$lower.bound), 6)
+  expect_equal(length(mod$error), 6)
+  expect_equal(length(mod$brier), 6)
+  expect_equal(mod$formula, mod.original$formula)
+
+})
+
+test_that("Update options", {
+
+  dat <- gen_mixture(10)
+  mod.original <- mod <- iprobit(dat$y, dat$X, control = list(maxit = 1),
+                                 silent = TRUE)
+
+  expect_message(mod <- iprobit(mod))
+  expect_output(mod <- iprobit(mod, stop.crit = 1e-1, maxit = 5, silent = FALSE))
+
+})
+
+test_that("The function update()", {
+
+  dat <- gen_mixture(10)
+  mod.original <- mod <- iprobit(dat$y, dat$X, control = list(maxit = 1),
+                                 silent = TRUE)
+  update(mod, maxit = 5)
+
+  expect_s3_class(mod, "iprobitMod")
+  expect_equal(mod$niter, 6)
+  expect_equal(length(mod$lower.bound), 6)
+  expect_equal(length(mod$error), 6)
+  expect_equal(length(mod$brier), 6)
+  expect_equal(mod$formula, mod.original$formula)
+
+})
